b064bf35efc9255feb15d395600e28fd430c88de
황윤진 <dbswlsghkd@gmail.com>
2024-09-03 19:39:22 +0900

    body-parser 모듈 사용

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,27 @@
 'use strict';
 
-const home = (req, res) => {
-    res.render('home/index');
+const output = {
+    // 렌더링 해주는 함수
+    home: (req, res) => {
+        res.render('home/index');
+    },
+
+    login: (req, res) => {
+        res.render('home/login');
+    },
 };
 
-const login = (req, res) => {
-    res.render('home/login');
+const process = {
+    // 렌더링 해주는 함수
+    login: (req, res) => {
+        console.log(req.body);
+    },
 };
 
 // object로 생성
 module.exports = {
-    home,
-    login,
+    output,
+    process,
     // hello : hello,
     // login : login,
     // (key : value) 구조를 단순화 시킬 수 있음
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 const ctrl = require('./home.ctrl');
 
-router.get('/', ctrl.home);
+router.get('/', ctrl.output.home);
+router.get('/login', ctrl.output.login);
 
-router.get('/login', ctrl.login);
-// router.post('/login', ctrl.login);
+router.post('/login', ctrl.process.login);
 
 // 외부에서 접근 가능하도록 사용하는 구문
 module.exports = router;

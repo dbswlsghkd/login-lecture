a5e3499b852cc70f0e7bf60fe4cf6db1a680f6ff
황윤진 <dbswlsghkd@gmail.com>
2024-09-03 19:16:15 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('hello');
+const id = document.querySelector('#id'),
+    psword = document.querySelector('#psword'),
+    loginBtn = document.querySelector('button');
+
+// console.log(id, psword, loginBtn);
+
+// const login = () => {
+//     console.log('bye');
+// };
+
+loginBtn.addEventListener('click', login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    console.log(req);
+}
